a9ede6e5b1cb8a297354c307bdaa8848a50453e5
Irina Sharaeva <irasharaeva@gmail.com>
2021-03-26 15:00:47 +0300

    feat: add solution "Dream team"

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,27 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  throw new CustomError('Not implemented');
-  // let answer = [];
-  // for(let i of members){
-  //   if(typeof(members[i]) === "string"){
-  //     answer.push(members[i].split('').filter(letter => letter !== ' ')[0]);
-  //   } else {
-  //     return false;
-  //   }
-  // }
-  // if(answer.length <= 1) return false;
-  // answer = answer.join('').toUpperCase().split('').sort();
-  // return answer;
-
-  // const ans = members.map((e) => {
-  //   if (members === undefined || members === null || arguments.length === 0 || members.length <= 1  ) return false;
-  //
-  //   if (typeof e === 'string') {
-  //     return e.trim().substring(0, 1).toUpperCase();
-  //   }
-  //   return ans.sort().join('');
-  // })
-
-
+  // throw new CustomError('Not implemented');
+  let res = [];
+  if (Array.isArray(members)) {
+    members.forEach((elem) => {
+      if (typeof elem === "string") {
+        res.push(elem.toUpperCase().trim().charAt());
+      } else {
+        return false;
+      }
+    });
+    return res.sort().join("");
+  } else {
+    return false;
+  }
 };
